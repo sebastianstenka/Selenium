3758495d5a21630e5d0f08d2da891edc9b6b1364
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-10-11 15:24:49 +0200

    Add project files.

---
+++ Selenium/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Selenium
+{
+    public class EntryPoint
+    {
+        private static void Main()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+
+            Thread.Sleep(3000);
+
+            driver.Quit();
+        }
+    }
+}
+ 
