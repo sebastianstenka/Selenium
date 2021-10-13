bc278ce330aed9c6f3cae82bf8e4fddd06e78bb3
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-10-13 09:45:38 +0200

    TextInputField

---
+++ Selenium/SpecialElements/TextInputField.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using Selenium.Selectors;
+using System.Threading;
+
+namespace Selenium.SpecialElements
+{
+    public class TextInputField
+    {
+        private const string SUBPAGE_NAME = "text-input-field";
+        private const string ELEMENT_NAME = "username";
+
+        public void Run()
+        {
+            var textBox = Selector.GetElement(SUBPAGE_NAME, By.Name(ELEMENT_NAME));
+            textBox.SendKeys("Test text");
+            Thread.Sleep(3000);
+            System.Console.WriteLine(textBox.GetAttribute("value"));
+            System.Console.WriteLine(textBox.GetAttribute("maxlength"));
+            textBox.Clear();
+            Thread.Sleep(3000);
+            Selector.Quit();
+        }
+    }
+}
