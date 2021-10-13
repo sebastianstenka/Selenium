de883b565237ef7c5fa3d926a18529fe7b5ba242
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-10-13 10:16:10 +0200

    RadioButton

---
+++ Selenium/SpecialElements/RadioButton.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using Selenium.Selectors;
+using System.Threading;
+
+namespace Selenium.SpecialElements
+{
+    public class RadioButton
+    {
+        private const string SUBPAGE_NAME = "radio-button-test";
+        private const string ELEMENT_NAME = "#post-10 > div > form > p:nth-child(6) > input[type=radio]:nth-child(1)";
+
+        public void Run()
+        {
+            var radioButton = Selector.GetElement(SUBPAGE_NAME, By.CssSelector(ELEMENT_NAME));
+            radioButton.Click();
+            Thread.Sleep(3000); 
+
+            var isChecked = radioButton.GetAttribute("checked") == "true";
+
+            Thread.Sleep(3000);
+            Selector.Quit();
+        }
+    }
+}
