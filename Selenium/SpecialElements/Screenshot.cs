6bae869bbb703e141b5c2600cacb7b92d0646806
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-10-18 16:12:08 +0200

    Screenshot

---
+++ Selenium/SpecialElements/Screenshot.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace Selenium.SpecialElements
+{
+    public class Screenshot
+    {
+        public void Make()
+        {
+            var chrome = new ChromeDriver();
+
+            chrome.Navigate().GoToUrl("https://www.google.com/");
+
+            var screenshot = chrome.GetScreenshot();
+
+            screenshot.SaveAsFile(Directory.GetCurrentDirectory() + ".png", OpenQA.Selenium.ScreenshotImageFormat.Png);
+
+            chrome.Quit();
+        }
+    }
+}
